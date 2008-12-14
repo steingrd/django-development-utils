e6754e0beae8a4e1b5c1d76fd0144ff6456614d2
Steingrim Dovland <steingrd@ifi.uio.no>
2008-12-14 19:51:45 +0100

    added Meta-class with verbose_name_plural to generated model for correctness in admin

--- development_utils/management/commands/createapp.py
+++ development_utils/management/commands/createapp.py
@@ -40,6 +40,9 @@ class Entry(models.Model):
     slug = models.SlugField()
     date = models.DateTimeField(auto_now_add=True)
     
+    class Meta:
+        verbose_name_plural = 'entries'
+
     def __unicode__(self):
         return self.title
 """
