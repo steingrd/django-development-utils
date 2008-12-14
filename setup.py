1251250f7fdeaa3f172909bc9ab7d358407a0e28
Steingrim Dovland <steingrd@ifi.uio.no>
2008-12-14 22:29:38 +0100

    made application distutils aware

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='django-development-utils',
+      version='0.1',
+      author='Steingrim Dovland',
+      author_email='steingrd@ifi.uio.no',
+      url='http://prettyprinted.net/code/django-development-utils/',
+      packages=['development_utils', 
+                'development_utils.management', 
+                'development_utils.management.commands']
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'])
